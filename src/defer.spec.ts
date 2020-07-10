56630cd28babe4c97f8c799b39c8070b50300957
Dima Parzhitsky <parzhitsky@gmail.com>
2020-07-10 15:36:55 +0300

    Add tests for defer()

---
+++ src/defer.spec.ts
@@ -0,0 +1,31 @@
+import defer from "./defer";
+
+it("should create a Promise-like object with exposed `.resolve()` and `.reject()`", () => {
+	const deferred = defer<42>();
+
+	expect(deferred).toBeInstanceOf(Promise);
+	expect(deferred).toHaveProperty("resolve", expect.any(Function));
+	expect(deferred).toHaveProperty("reject", expect.any(Function));
+});
+
+it("should allow manually resolving the promise", (done) => {
+	const deferred = defer<42>();
+
+	deferred
+		.then((value) => expect(value).toBe(42))
+		.finally(done);
+
+	deferred.resolve(42);
+});
+
+it("should allow manually rejecting the promise", (done) => {
+	const error = new Error("Unexpected error");
+
+	const deferred = defer<any>();
+
+	deferred
+		.catch((caught: Error) => expect(caught).toBe(error))
+		.finally(done);
+
+	deferred.reject(error);
+});
