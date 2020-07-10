d7e64a9f50001d070377d9cc99dbe5322d2d3237
Dima Parzhitsky <parzhitsky@gmail.com>
2020-07-10 15:14:37 +0300

    Use Jest

---
+++ jest.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  preset: "ts-jest",
+  testEnvironment: "node",
+  clearMocks: true,
+
+  rootDir: ".",
+  testMatch: [
+    "<rootDir>/src/**/*.{spec,test}.ts",
+  ],
+
+  collectCoverage: true,
+  collectCoverageFrom: [
+    "<rootDir>/src/**/*.ts",
+    "!<rootDir>/{src,test}/**/*.{spec,test}.ts",
+  ],
+  coverageDirectory: "coverage",
+  coverageProvider: "v8",
+  coverageReporters: [
+    "text",
+    "lcov",
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100,
+    },
+  },
+};
