a9a58b47dd40e59483d6c43ec69f981294af9b59
Dmytro Parzhytskyi <parzhitsky@gmail.com>
2024-07-12 17:09:25 +0300

    Add linting

--- src/defer.spec.ts
+++ src/defer.spec.ts
@@ -1,31 +1,36 @@
-import defer from "./defer";
+import defer from './defer'
 
-it("should create a Promise-like object with exposed `.resolve()` and `.reject()`", () => {
-	const deferred = defer<42>();
+describe(defer, () => {
+  it('should create a Promise-like object with exposed `.resolve()` and `.reject()`', () => {
+    const deferred = defer<42>()
 
-	expect(deferred).toBeInstanceOf(Promise);
-	expect(deferred).toHaveProperty("resolve", expect.any(Function));
-	expect(deferred).toHaveProperty("reject", expect.any(Function));
-});
+    expect(deferred).toBeInstanceOf(Promise)
+    expect(deferred).toHaveProperty('resolve', expect.any(Function))
+    expect(deferred).toHaveProperty('reject', expect.any(Function))
+  })
 
-it("should allow manually resolving the promise", (done) => {
-	const deferred = defer<42>();
+  it('should allow manually resolving the promise', (done) => {
+    const deferred = defer<42>()
 
-	deferred
-		.then((value) => expect(value).toBe(42))
-		.finally(done);
+    void deferred
+      .then((value) => {
+        expect(value).toBe(42)
+      })
+      .finally(done)
 
-	deferred.resolve(42);
-});
+    deferred.resolve(42)
+  })
 
-it("should allow manually rejecting the promise", (done) => {
-	const error = new Error("Unexpected error");
+  it('should allow manually rejecting the promise', (done) => {
+    const error = new Error('Unexpected error')
+    const deferred = defer<unknown>()
 
-	const deferred = defer<any>();
+    void deferred
+      .catch((caught: Error) => {
+        expect(caught).toBe(error)
+      })
+      .finally(done)
 
-	deferred
-		.catch((caught: Error) => expect(caught).toBe(error))
-		.finally(done);
-
-	deferred.reject(error);
-});
+    deferred.reject(error)
+  })
+})
