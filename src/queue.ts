392a2a005c852dc1c897d14a64f273dd006780ff
Dmytro Parzhytskyi <parzhitsky@gmail.com>
2026-05-30 11:08:41 +0300

    Extract pruning functionality into a separate class

--- src/queue.ts
+++ src/queue.ts
@@ -1,38 +1,49 @@
 function scheduleIdleCallback(callback: () => void): void {
   // eslint-disable-next-line @typescript-eslint/no-unused-expressions
-  globalThis.requestIdleCallback?.(callback)
-  ?? globalThis.setImmediate?.(callback)
-  ?? globalThis.setTimeout?.(callback)
+  globalThis.requestIdleCallback?.(callback) ?? globalThis.setImmediate?.(callback) ?? globalThis.setTimeout?.(callback)
+}
+
+class Shared<Value> {
+  constructor(public value: Value) {}
+}
+
+class Pruner<Item> {
+  protected cleanupScheduled = false
+
+  constructor(
+    protected readonly items: Item[],
+    protected readonly start: Shared<number>,
+  ) {}
+
+  protected prune() {
+    for (let i = this.start.value; i < this.items.length; i++) {
+      this.items[i - this.start.value] = this.items[i]
+    }
+
+    this.items.length -= this.start.value
+    this.start.value = 0
+    this.cleanupScheduled = false
+  }
+
+  schedulePruneIfNeeded() {
+    if (!this.cleanupScheduled && this.start.value >= this.items.length / 2) {
+      scheduleIdleCallback(() => this.prune())
+      this.cleanupScheduled = true
+    }
+  }
 }
 
 export class Queue<Item> {
   protected readonly items: Item[] = []
-  protected start = 0
-  private cleanupScheduled = false
+  protected readonly start = new Shared(0)
+  protected readonly pruner = new Pruner(this.items, this.start)
 
   get isEmpty(): boolean {
-    return this.start >= this.items.length
+    return this.start.value >= this.items.length
   }
 
   enqueue(item: Item): void {
     this.items.push(item)
-  }
-
-  private prune() {
-    for (let i = this.start; i < this.items.length; i++) {
-      this.items[i - this.start] = this.items[i]
-    }
-
-    this.items.length -= this.start
-    this.start = 0
-    this.cleanupScheduled = false
-  }
-
-  private schedulePruneIfNeeded() {
-    if (!this.cleanupScheduled && this.start >= this.items.length / 2) {
-      scheduleIdleCallback(() => this.prune())
-      this.cleanupScheduled = true
-    }
   }
 
   dequeue(): Item | undefined {
@@ -40,11 +51,11 @@ export class Queue<Item> {
       return undefined
     }
 
-    const item = this.items[this.start]
+    const item = this.items[this.start.value]
 
-    delete this.items[this.start++] // eslint-disable-line @typescript-eslint/no-array-delete
+    delete this.items[this.start.value++] // eslint-disable-line @typescript-eslint/no-array-delete
 
-    this.schedulePruneIfNeeded()
+    this.pruner.schedulePruneIfNeeded()
 
     return item
   }
