f32c37feba9fef014c221f479b09c4d5ec099fc1
Dmytro Parzhytskyi <parzhitsky@gmail.com>
2026-05-31 14:19:58 +0300

    Validate `maxDanglingItems`

--- src/queue.ts
+++ src/queue.ts
@@ -2,6 +2,8 @@ import { Slacker } from './slacker.js'
 
 export const DEFAULT_MAX_DANGLING_ITEMS = 100
 
+const MAX_ARRAY_LENGTH = 2 ** 32 - 1
+
 class Shared<Value> {
   constructor(public value: Value) {}
 }
@@ -24,9 +26,25 @@ class Pruner<Item> {
   }
 }
 
+function validateMaxDanglingItems(input: number): number {
+  if (input > MAX_ARRAY_LENGTH && input < Infinity) {
+    console.warn(`The provided dangling items limit (${input}) is too big and will be treated as Infinity (i.e., no pruning).`)
+    // actually, it's going to be treated as MAX_ARRAY_LENGTH, but nobody needs to know about that 🤫
+  }
+
+  input = Math.min(input, MAX_ARRAY_LENGTH)
+
+  if (input <= 0 || !Number.isInteger(input)) {
+    throw new Error(`Invalid maxDanglingItems: expected a valid array length, got ${input}`)
+  }
+
+  return input
+}
+
 export interface QueueParams {
   /**
    * The number of items removed from the queue after which queue pruning is scheduled.
+   * To disable pruning set this to `Infinity`.
    * Defaults to {@link DEFAULT_MAX_DANGLING_ITEMS}.
    */
   readonly maxDanglingItems?: number
@@ -37,9 +55,9 @@ export class Queue<Item> {
   protected readonly start = new Shared(0)
   protected readonly pruner = new Pruner(this.items, this.start)
   protected readonly prunerLazy = new Slacker(() => this.pruner.prune())
-  protected readonly maxDanglingItems = this.params?.maxDanglingItems ?? DEFAULT_MAX_DANGLING_ITEMS
+  protected readonly maxDanglingItems = validateMaxDanglingItems(this.params?.maxDanglingItems ?? DEFAULT_MAX_DANGLING_ITEMS)
   protected readonly maybePruneWhenIdle =
-    this.maxDanglingItems === Infinity
+    this.maxDanglingItems === MAX_ARRAY_LENGTH
       ? () => {}
       : () => {
         if (this.start.value >= this.maxDanglingItems) {
