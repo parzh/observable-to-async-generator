0ba9d358bb960199f479e758e11d5d96e33b490a
Dmytro Parzhytskyi <parzhitsky@gmail.com>
2026-06-07 21:10:23 +0300

    Add browser tests

---
+++ vitest.browser.config.ts
@@ -0,0 +1,17 @@
+import { playwright } from '@vitest/browser-playwright'
+import { mergeConfig } from 'vitest/config'
+import baseConfig from './vitest.config.js'
+
+export default mergeConfig(baseConfig, {
+  test: {
+    browser: {
+      enabled: true,
+      provider: playwright(),
+      instances: [
+        { browser: 'firefox' },
+        { browser: 'webkit' },
+        { browser: 'chromium' },
+      ],
+    },
+  },
+})
