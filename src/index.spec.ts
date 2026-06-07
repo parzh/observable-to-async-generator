ef3ae03457ee38db45c3925003a29fcfeabf6718
Dmytro Parzhytskyi <parzhitsky@gmail.com>
2026-06-07 22:43:45 +0300

    Remove outdated test case

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,5 +1,4 @@
 import { describe, expect, it } from 'vitest'
-import { defer } from './flow/defer.js'
 import { otag } from './otag.js'
 import * as indexfile from './index.js'
 
@@ -10,9 +9,5 @@ describe('default namespace (indexfile)', () => {
 
   it('should contain the `otag` function as a named export', () => {
     expect(indexfile.otag).toBe(otag)
-  })
-
-  it('should contain the `defer` function as a named export', () => {
-    expect(indexfile.defer).toBe(defer)
   })
 })
